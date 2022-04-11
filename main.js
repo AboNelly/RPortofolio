6ef547813e6101c8d2cfeb7d53d5d7d1af465aaf
AboNelly <91729878+AboNelly@users.noreply.github.com>
2022-04-11 19:17:26 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+/*let pic = document.getElementById(hiddenclickimg);
+let word = document.getElementById(hiddenclick);
+
+function showPic(){
+  pic.hidden = 'false' ;
+  word.style.color = 'red';
+  word.style.backgroundColor = 'blue';
+}
+
+
+word.onclick = showPic;
+*/
+/*
+let  imagehidden =false
+let pic = document.getElementById('hiddenclickimg');
+let word = document.getElementById('hiddenclick');
+
+function showPic(){
+if(imagehidden){
+  pic.hidden = false ;
+  word.style.color = 'red';
+  word.style.backgroundColor = 'blue';
+  imagehidden=false
+  }
+  else{
+   pic.hidden =true;
+   imagehidden=true
+  }
+}
+*/
+/*
+const image = document.getElementById('hiddenclickimg');
+
+const imageClick = () => {
+   image.classList.toggle('hidden');
+}*/
+
+
+
+function showPic1() {
+  const pic = document.getElementById("hide1");
+  if (pic.style.display === "none") {
+    pic.style.display = "block";
+  } else  {
+    pic.style.display = "none";
+  }
+}
+
+function showPic2() {
+  const pic = document.getElementById("hide2");
+  if (pic.style.display === "none") {
+    pic.style.display = "block";
+  } else {
+    pic.style.display = "none";
+  }
+}
+
+function showPic3() {
+  const pic = document.getElementById("hide3");
+  if (pic.style.display === "none") {
+    pic.style.display = "block";
+  } else {
+    pic.style.display = "none";
+  }
+}
+
+function showPic4() {
+  const pic = document.getElementById("hide4");
+  if (pic.style.display === "none") {
+    pic.style.display = "block";
+  } else {
+    pic.style.display = "none";
+  }
+}
+
+
+
